dcd36324f2572f8bf99388e816d50f490804241c
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-20 14:28:08 +0200

    Update tds_raindrop_localization.py

--- tds_report/tds_report/report/tds_raindrop_localization/tds_raindrop_localization.py
+++ tds_report/tds_report/report/tds_raindrop_localization/tds_raindrop_localization.py
@@ -53,13 +53,16 @@ def execute(filters=None):
 		purchase_invoice = frappe.db.get_list("Purchase Invoice", filters={"supplier":sup.name}, fields=['*'])
 		for pur in purchase_invoice:
 			tds = frappe.db.get_all("Purchase Taxes and Charges", filters={"parent":pur.name, "add_deduct_tax":"Deduct" , "custom_when_to_use":filters.account_head}, fields=['*'])
-			for t in tds:
-				total_tds_amount += t.tax_amount
+			if tds != []:
+				for t in tds:
+					total_tds_amount += t.tax_amount
 			journal = frappe.db.get_list("Journal Entry", fields=['*'])
-			# for jour in journal:
-			# 	journal_tds = frappe.db.get_all("Journal Entry Account", filters={"parent":jour.name,"custom_when_to_use":filters.account_head, "reference_type":"Purchase Invoice", "reference_name":pur.name}, fields=['*'])
-			# 	for tds in journal_tds:
-			# 		total_tds_paid_amount += tds.debit
+			if journal != []:
+				for jour in journal:
+					journal_tds = frappe.db.get_all("Journal Entry Account", filters={"parent":jour.name,"custom_when_to_use":filters.account_head, "reference_type":"Purchase Invoice", "reference_name":pur.name}, fields=['*'])
+					if journal_tds ! = []:
+						for tds in journal_tds:
+							total_tds_paid_amount += tds.debit
 		
 		data.append([sup.name, filters.account_head, total_tds_amount, total_tds_paid_amount, total_tds_balance_amount])
 	return columns, data
