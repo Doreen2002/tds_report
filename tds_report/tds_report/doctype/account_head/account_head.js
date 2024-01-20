4a93708da65a74480335b4a6a718e2faa3152580
Doreen2002 <doreenmwapekatebe8@gmail.com>
2024-01-20 09:58:08 +0000

    added tds account head

---
+++ tds_report/tds_report/doctype/account_head/account_head.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, raindrop and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Account Head', {
+	// refresh: function(frm) {
+
+	// }
+});
