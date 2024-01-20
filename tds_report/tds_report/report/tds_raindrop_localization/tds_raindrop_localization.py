8031de11f293a4e1c622c60813c1df9a5e0376ac
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-20 11:11:24 +0200

    Update tds_raindrop_localization.py

--- tds_report/tds_report/report/tds_raindrop_localization/tds_raindrop_localization.py
+++ tds_report/tds_report/report/tds_raindrop_localization/tds_raindrop_localization.py
@@ -6,4 +6,39 @@
 
 def execute(filters=None):
 	columns, data = [], []
+	columns =[
+	{
+            'fieldname': 'Supplier',
+            'label': _('Supplier'),
+            'fieldtype': 'Link',
+	    'option': 'Supplier',
+           
+        },
+	{
+            'fieldname': 'account_head',
+            'label': _('Account Head'),
+            'fieldtype': 'Link',
+	    'options': 'Account Head'
+           
+        },
+	{
+            'fieldname': 'tds_amount',
+            'label': _('TDs Amount'),
+            'fieldtype': 'Currency',
+           
+        },
+	{
+            'fieldname': 'paid_amount',
+            'label': _('Paid Amount'),
+            'fieldtype': 'Currency',
+        },
+	{
+            'fieldname': 'tds_balance',
+            'label': _('Paid Amount'),
+            'fieldtype': 'Currency',
+		
+	   
+           
+        }
+	]
 	return columns, data
