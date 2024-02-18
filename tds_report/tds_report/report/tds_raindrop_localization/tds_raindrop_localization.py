81924aa688ea98aa4f292bd63d7610fce78d463c
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-18 12:11:49 +0200

    Update tds_raindrop_localization.py

--- tds_report/tds_report/report/tds_raindrop_localization/tds_raindrop_localization.py
+++ tds_report/tds_report/report/tds_raindrop_localization/tds_raindrop_localization.py
@@ -97,16 +97,21 @@ def execute(filters=None):
 		total_tds_amount = 0
 		total_tds_paid_amount = 0
 		total_tds_balance_amount = 0
+		journal_voucher =" "
+		submitted_number = ""
 		account = filters.account_head
 		tds = frappe.db.get_all("Purchase Taxes and Charges", filters={"parent":pur.name, "add_deduct_tax":"Deduct", "custom_when_to_use" : filters.account_head}, fields=['*'])
 		for t in tds:
 			if t.add_deduct_tax == "Deduct" and "TDS" in t.account_head:
 				total_tds_amount += t.tax_amount
-			journal = frappe.db.get_list("Journal Entry", fields=['*'])
-			for jour in journal:
-				journal_tds = frappe.db.get_all("Journal Entry Account", filters={"parent":jour.name,"custom_when_to_use":filters.account_head, "reference_type":"Purchase Invoice", "reference_name":pur.name}, fields=['*'])
+			journal_tds = frappe.db.get_all("Journal Entry Account", filters={"custom_tds_category":filters.account_head, "custom_submitted_number": pur.name}, fields=['*'])
+			if journal_tds != []:
 				for tds in journal_tds:
 					total_tds_paid_amount += tds.debit
 		total_tds_balance_amount =  total_tds_paid_amount -  total_tds_amount 
-		data.append([pur.supplier, filters.account_head, total_tds_amount, total_tds_paid_amount, total_tds_balance_amount , pur.posting_date, pur.name, "", ""])
+		if frappe.db.get_value("Journal Entry Account", {"custom_tds_category":filters.account_head, "custom_submitted_number": pur.name}, 'parent') != None:
+			journal_voucher = frappe.db.get_value("Journal Entry Account", {"custom_tds_category":filters.account_head, "custom_submitted_number": pur.name}, 'parent')
+		if frappe.db.get_value("Journal Entry Account", {"custom_tds_category":filters.account_head, "custom_submitted_number": pur.name}, 'custom_submitted_number') != None:
+			submitted_number = frappe.db.get_value("Journal Entry Account", {"custom_tds_category":filters.account_head, "custom_submitted_number": pur.name}, 'custom_submitted_number')
+		data.append([pur.supplier, filters.account_head, total_tds_amount, total_tds_paid_amount, total_tds_balance_amount , pur.posting_date, pur.name, journal_voucher, submitted_number])
 	return columns, data
