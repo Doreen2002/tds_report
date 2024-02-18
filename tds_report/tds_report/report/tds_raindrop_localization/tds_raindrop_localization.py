e484987f90208835c8354d0a5a378a8c26d526ed
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-18 11:33:57 +0200

    Update tds_raindrop_localization.py

--- tds_report/tds_report/report/tds_raindrop_localization/tds_raindrop_localization.py
+++ tds_report/tds_report/report/tds_raindrop_localization/tds_raindrop_localization.py
@@ -91,7 +91,7 @@ def execute(filters=None):
 		account = filters.account_head
 		filters_list = {"parent":pur.name, "add_deduct_tax":"Deduct", "custom_when_to_use" : filters.account_head}
 		if filters.supplier != None:
-			filters.update({"supplier": filters.supplier})
+			filters_list.update({"supplier": filters.supplier})
 		tds = frappe.db.get_all("Purchase Taxes and Charges", filters=filters_list, fields=['*'])
 		for t in tds:
 			if t.add_deduct_tax == "Deduct" and "TDS" in t.account_head:
